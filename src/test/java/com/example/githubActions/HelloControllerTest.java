a1ad77d866adfc76b162cc5cdb73db6413d316e5
Crystal Hansen <59895987+cryshansen@users.noreply.github.com>
2025-10-22 16:21:48 -0400

    Update HelloControllerTest.java
    change to test controller not endpoint

--- src/test/java/com/example/githubActions/HelloControllerTest.java
+++ src/test/java/com/example/githubActions/HelloControllerTest.java
@@ -3,12 +3,15 @@ package com.example.githubActions;
 
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.githubActions.controller.HelloController;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,12 +19,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class HelloControllerTest {
 
-    
+	 @Test
+	    void testHelloEndpoint() {
+	        HelloController controller = new HelloController();
+	        String response = controller.sayHello();
+	        assertEquals("Hello, GitHub Actions!", response);
+	    }
 
-    @Test
+   /* @Test
     void testHelloEndpoint() {
     	 RestTemplate restTemplate = new RestTemplate(); // 👈 create manually
         ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/hello", String.class);
         assertThat(response.getBody()).isEqualTo("Hello, GitHub Actions!");
-    }
+    }*/
 }
